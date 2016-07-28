4cecfb17a2b39a7168d7e3084cd05fb0a8451021
amellomenezes <ander@HM15>
2016-07-28 16:06:32 -0300

    Add project files.

---
+++ minhapp1/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(minhapp1.Startup))]
+namespace minhapp1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
